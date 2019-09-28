95648827f5f7d44bfdc422ac5cdb96cd13d6e415
Dani Dura <danieldura7@gmail.com>
2019-09-28 11:28:07 +0200

    Initial Commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Daniel Dura Monge on 28/09/2019.
+//  Copyright © 2019 Daniel Dura Monge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
